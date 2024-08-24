b450a7796473f77915aa87749edb66852aa34108
Swordfish189 <flagman05@gmail.com>
2024-08-24 16:23:05 +0200

    black now working too - fully usable (still cluttered though)

--- init.lua
+++ init.lua
@@ -139,6 +139,30 @@ require("lazy").setup({
       name = "rose-pine",
       config = function() vim.cmd("colorscheme rose-pine") end,
     },
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        local null_ls = require("null-ls")
+        local formatting = null_ls.builtins.formatting
+        null_ls.setup({
+          sources = {
+            formatting.black,
+          },
+          on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+              })
+            end
+          end,
+        })
+      end,
+    },
     "theprimeagen/harpoon",
     "mbbill/undotree",
     {
